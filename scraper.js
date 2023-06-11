38aaaa6dfbd3c8d49e7de329463ae133c52bf68e
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2023-06-10 20:25:54 -0500

    feae

--- scraper.js
+++ scraper.js
@@ -422,7 +422,8 @@ async function borrarReplicas() {
 async function scrapeMadTactical(url, tiendaNombre, categoria, estado) {
   const browser = await puppeteer.launch({
     headless: false,
-    ignoreDefaultArgs: ["--disable-extensions"],
+    executablePath:
+      "./node_modules/puppeteer/.local-chromium/win64-656675/chrome-win/chrome.exe",
   });
 
   let newProductIds = [];
